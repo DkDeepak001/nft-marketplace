becc653939e2c0f14245306eeed30693b1d17b0d
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-26 20:14:47 +0530

    feat: :fire: tailwind setup

--- apps/thirdweb-app/pages/_app.tsx
+++ apps/thirdweb-app/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
