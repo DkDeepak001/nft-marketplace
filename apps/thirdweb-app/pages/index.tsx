48c2ab49077f0d170edbfc369a91b0329cb02f81
DkDeepak001 <Deepakeswar001@gmail.com>
2023-05-28 18:27:30 +0530

    fix: :zap: render all nfts from nft-collection

--- apps/thirdweb-app/pages/index.tsx
+++ apps/thirdweb-app/pages/index.tsx
@@ -1,18 +1,14 @@
-import { useActiveListings, useContract } from "@thirdweb-dev/react";
+import { useContract, useNFTs } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Header from "../components/header";
 import CardSkeleton from "../components/skeleton/card";
 import Card from "../components/card/card";
-
-const MARKET_PLACE_ADDRESS = "0x53E5eF7824d694b68A04C9458e51e7460e64b36e";
+import { COLLECTION_ADDRESS, MARKET_PLACE_ADDRESS } from "./address";
 
 const Home: NextPage = () => {
-  const { contract } = useContract(MARKET_PLACE_ADDRESS, "marketplace");
+  const { contract } = useContract(COLLECTION_ADDRESS);
+  const { data: nfts, isLoading } = useNFTs(contract);
 
-  const { data: nfts, isLoading } = useActiveListings(contract);
-  // const { data: nfts, isLoading } = useNFTs(contract);
-
-  // console.log(data);
   return (
     <div className="bg-brand-primary  max-h-full min-h-screen  px-32 items-center">
       <Header />
@@ -25,7 +21,7 @@ const Home: NextPage = () => {
             No NFT's available for listing
           </h1>
         ) : (
-          nfts?.map((item) => <Card item={item} contract={contract} />)
+          nfts?.map((item) => <Card item={item} id={item.metadata.id} />)
         )}
       </div>
     </div>
