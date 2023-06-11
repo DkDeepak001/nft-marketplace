048f9a483e0ee65f49cb1a3dbcbda8b8e256a7b6
DkDeepak001 <Deepakeswar001@gmail.com>
2023-06-11 19:00:45 +0530

    feat : add vercel analytics

--- apps/thirdweb-app/pages/_app.tsx
+++ apps/thirdweb-app/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import { Amaranth } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 //font setyop
 const amaranth = Amaranth({
@@ -22,6 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className={`${amaranth.variable} font-amarnath`}>
         <Toaster />
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ThirdwebProvider>
   );
